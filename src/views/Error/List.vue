ab550eb7500310ab5e32393d27041391b65ab003
Luis Fernando Carvalho <luiscvlh11@gmail.com>
2019-09-19 12:05:54 -0300

    Obter todos os elementos da api e ajeitar table

--- src/views/Error/List.vue
+++ src/views/Error/List.vue
@@ -1,21 +1,31 @@
 <template>
   <NavBar>
     <h1 class="text-center">Listagem de erros</h1>
+    <h1>Total de elementos na página atual {{numberOfElements}}</h1>
+    <h1>Total de elementos na base {{totalElements}}</h1>
+    <h1>Total de paginas {{totalPages}}</h1>
     <v-data-table
       :headers="headers"
       :items="items"
-      :items-per-page="10"
+      :items-per-page="20"
       class="elevation-24"
       no-data-text="Nenhum log disponível"
       loading-text="Carregando logs..."
       :progress="loading"
       :loading="loading"
+      dense
     >
       <template slot="item.level" slot-scope="props">
-        <td>{{props.item.detail.level}}</td>
+        <td>
+          <v-btn
+            width="100"
+            depressed
+            :color="getColor(props.item.detail.level)"
+          >{{props.item.detail.level}}</v-btn>
+        </td>
       </template>
       <template slot="item.title" slot-scope="props">
-        <td width="10px;">{{props.item.title}}</td>
+        <td>{{props.item.title}}</td>
       </template>
       <template slot="item.ip" slot-scope="props">
         <td>{{props.item.application.ip}}</td>
@@ -43,20 +53,23 @@ import NavBar from "@/components/Navbar";
 export default {
   data() {
     return {
-      text: "",
       loading: true,
+      totalPages: 0,
+      totalElements: 0,
+      lastPage: false,
+      firstPage: true,
+      numberOfElements: 0,
       headers: [
-        { text: "Nível", align: "center", value: "level", width: "10%" },
-        { text: "Título", align: "center", value: "title", width: "50%" },
-        { text: "IP", align: "center", value: "ip", width: "10%" },
+        { text: "Nível", align: "center", value: "level" },
+        { text: "Título", align: "center", value: "title" },
+        { text: "IP", align: "center", value: "ip" },
         {
           text: "Aplicação",
           align: "center",
-          value: "application",
-          width: "10%"
+          value: "application"
         },
-        { text: "Criado por", align: "center", value: "author", width: "10%" },
-        { text: "Criado em", align: "center", value: "timestamp", width: "10%" }
+        { text: "Criado por", align: "center", value: "author" },
+        { text: "Criado em", align: "center", value: "timestamp" }
       ],
       items: []
     };
@@ -64,8 +77,35 @@ export default {
   components: { NavBar },
   async created() {
     const { data } = await axios.get(`${api_logs}`);
-    this.items = data.content;
     this.loading = false;
+    this.totalPages = data.totalPages;
+    this.totalElements = data.totalElements;
+    this.items = data.content;
+
+    let i, j;
+    for (i = 1; i < this.totalPages; i++) {
+      const { data } = await axios.get(`${api_logs}?page=${i}`);
+      this.lastPage = data.last;
+      this.firstPage = data.first;
+      this.numberOfElements = data.numberOfElements;
+      for (j = 0; j < data.content.length; j++) {
+        this.items.push(data.content[j]);
+      }
+      console.log(`oi ${i}`);
+    }
+    console.log(this.items);
+  },
+  methods: {
+    getColor(level) {
+      switch (level) {
+        case "ERROR":
+          return "error";
+        case "WARNING":
+          return "warning";
+        case "DEBUG":
+          return "primary";
+      }
+    }
   }
 };
 </script>

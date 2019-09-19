1249750644ee2e4a50530e4920dafbdc2d9886e9
Luis Fernando Carvalho <luiscvlh11@gmail.com>
2019-09-19 16:45:31 -0300

    Modificar modal adicao log

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -35,44 +35,28 @@
         <slot></slot>
       </v-container>
     </v-content>
-    <v-btn bottom color="pink" dark fab fixed right @click="dialog = !dialog">
+    <v-btn bottom color="error" dark fab fixed right @click="dialog = !dialog">
       <v-icon>mdi-plus</v-icon>
     </v-btn>
     <v-dialog v-model="dialog" width="800px">
       <v-card>
-        <v-card-title class="grey darken-2">Create contact</v-card-title>
+        <v-card-title class="blue darken-3 white--text">Adicionar log</v-card-title>
         <v-container>
           <v-row>
             <v-col class="align-center justify-space-between" cols="12">
-              <v-row align="center">
-                <v-avatar size="40px" class="mr-4">
-                  <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt />
-                </v-avatar>
-                <v-text-field placeholder="Name"></v-text-field>
-              </v-row>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field prepend-icon="business" placeholder="Company"></v-text-field>
-            </v-col>
-            <v-col cols="6">
-              <v-text-field placeholder="Job title"></v-text-field>
+              <v-text-field placeholder="Título"></v-text-field>
             </v-col>
             <v-col cols="12">
-              <v-text-field prepend-icon="mail" placeholder="Email"></v-text-field>
+              <v-text-field placeholder="Level"></v-text-field>
             </v-col>
             <v-col cols="12">
-              <v-text-field type="tel" prepend-icon="phone" placeholder="(000) 000 - 0000"></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-text-field prepend-icon="notes" placeholder="Notes"></v-text-field>
+              <v-text-field placeholder="Aplicação"></v-text-field>
             </v-col>
           </v-row>
         </v-container>
         <v-card-actions>
-          <v-btn text color="primary">More</v-btn>
-          <div class="flex-grow-1"></div>
-          <v-btn text color="primary" @click="dialog = false">Cancel</v-btn>
-          <v-btn text @click="dialog = false">Save</v-btn>
+          <v-btn text color="error" @click="dialog = false">Cancelar</v-btn>
+          <v-btn text @click="dialog = false" color="success">Adicionar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>

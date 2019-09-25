fe66770554f0510cd615d8e42c4088ef06e9a275
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-25 08:39:49 -0300

    Incluir filtro para formatação da data

--- src/filters/index.js
+++ src/filters/index.js
@@ -11,3 +11,18 @@ Vue.filter('environment', (value) => {
   if (!value) return ''
   return environment.filter((l) => l.value === value).map((l) => { return l.text })[0]
 })
+
+
+Vue.filter('date', (value) => {
+  if (!value) return ''
+  const options = { 
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+  }
+  const date = new Date(value)
+  return date.toLocaleDateString(['pt-BR'], options)
+})

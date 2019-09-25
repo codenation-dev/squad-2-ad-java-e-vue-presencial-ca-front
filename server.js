320662d66329402cbbd0e6de0dbad0f371e91b13
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-25 11:26:26 -0300

    fix express init app

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const serveStatic = require("serve-static")
-const path = require('path')
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
 
-const app = express();
+var app = express()
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+app.get('*', (req, res) => {
+  res.sendFile(`${__dirname}/dist/index.html`)
+})
+
+var port = process.env.PORT || 5000
+app.listen(port)
+
+// eslint-disable-next-line no-console
+console.log('server started ' + port)

d0fafac758f7e5d65a3b22092d6d5dc99a056b89
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-26 09:07:28 -0300

    Melhorias na tela das triggers

--- src/views/Triggers/List.vue
+++ src/views/Triggers/List.vue
@@ -1,120 +1,141 @@
 <template>
   <v-container grid-list-xs>
-    <v-data-table
-      :headers="headers"
-      :items="formatTriggers"
-      :loading="isLoading"
-      class="elevation-1"
-    >
-      <template v-slot:top>
-        <v-toolbar flat color="white">
-          <div class="flex-grow-1"></div>
-          <v-dialog :dark="isDark" v-model="dialog" max-width="800px">
-            <template v-slot:activator="{ on }">
-              <v-btn color="primary" depressed class="mp-5" v-on="on">Create Trigger</v-btn>
-            </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
-              </v-card>
-              <v-tabs grow background-color="primary" dark>
-                <v-tab>Geral</v-tab>
-                <v-tab>Filters</v-tab>
-                <v-tab>Email</v-tab>
 
-                <v-tab-item>
-                  <v-card>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="12">
-                            <v-text-field v-model="editedItem.name" label="Trigger Name" outlined></v-text-field>
-                          </v-col>
-                          <v-col cols="12">
-                            <v-switch v-model="editedItem.isActive" label="Active?"></v-switch>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                  </v-card>
-                </v-tab-item>
-                <v-tab-item>
-                  <v-card>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="12">
-                            <v-text-field v-model="editedItem.filters.appName"  label="Application Name" outlined ></v-text-field>
-                          </v-col>
-                          <v-col cols="12" sm="6">
-                            <v-select
-                              v-model="editedItem.filters.level"
-                              :items="levels"
-                              item-text="text"
-                              item-value="value"
-                              label="Levels"
-                              outlined
-                            ></v-select>
-                          </v-col>
-                          <v-col cols="12" sm="6">
-                            <v-select
-                              v-model="editedItem.filters.environment"
-                              :items="environments"
-                              item-text="text"
-                              item-value="value"
-                              label="Environments"
-                              outlined
-                            ></v-select>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                  </v-card>
-                </v-tab-item>
+    <div id="title">
+      <h1 class="pa-5 font-weight-light">Triggers Management</h1>
+    </div>
+    
+    <div id="triggers">
+      <v-data-table
+        :headers="headers"
+        :items="formatTriggers"
+        :loading="isLoading"
+        class="elevation-1"
+      >
+        <template v-slot:top>
+          <v-toolbar flat color="white">
+            <div class="flex-grow-1"></div>
+            <v-dialog v-model="dialog" max-width="800px">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" depressed class="mp-5" v-on="on">Create Trigger</v-btn>
+              </template>
+                <v-card >
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
+                </v-card>
+                <v-tabs grow background-color="primary" dark>
+                  <v-tab>
+                    <v-icon class="pr-2">mdi-numeric-1-circle</v-icon>
+                    Geral
+                  </v-tab>
+                  <v-tab>
+                    <v-icon class="pr-2">mdi-numeric-2-circle</v-icon>
+                    Filters
+                  </v-tab>
+                  <v-tab>
+                    <v-icon class="pr-2">mdi-numeric-3-circle</v-icon>
+                    Email
+                  </v-tab>
+                  <v-tab-item>
+                    <v-card>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12">
+                              <v-text-field v-model="editedItem.name" label="Trigger Name" outlined></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-switch v-model="editedItem.isActive" label="Active?"></v-switch>
+                            </v-col>
+                          </v-row>
+                        </v-container>
+                      </v-card-text>
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-card>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12">
+                              <v-text-field v-model="editedItem.filters.appName"  label="Application Name" outlined ></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                              <v-select
+                                v-model="editedItem.filters.level"
+                                :items="levels"
+                                item-text="text"
+                                item-value="value"
+                                label="Levels"
+                                outlined
+                              ></v-select>
+                            </v-col>
+                            <v-col cols="12" sm="6">
+                              <v-select
+                                v-model="editedItem.filters.environment"
+                                :items="environments"
+                                item-text="text"
+                                item-value="value"
+                                label="Environments"
+                                outlined
+                              ></v-select>
+                            </v-col>
+                          </v-row>
+                        </v-container>
+                      </v-card-text>
+                    </v-card>                  
+                  </v-tab-item>
 
-                <v-tab-item>
-                  <v-card>
-                    <v-card-text>
-                      <v-container>
-                        <v-row>
-                          <v-col cols="12">
-                            <v-text-field v-model="editedItem.email"  label="Email" outlined ></v-text-field>
-                          </v-col>
-                          <v-col cols="12">
-                            <v-textarea outlined label="Message" v-model="editedItem.message"></v-textarea>
-                          </v-col>
-                        </v-row>
-                      </v-container>
-                    </v-card-text>
-                    <v-card-actions>
-                      <div class="flex-grow-1"></div>
-                      <v-btn color="red" text @click="close">Cancel</v-btn>
-                      <v-btn color="primary" text @click="save">Save</v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-tab-item>
-              </v-tabs>
-          </v-dialog>
-        </v-toolbar>
-      </template>
-      <template v-slot:item.filtersLevel="{ item }">
-        {{item.filtersLevel | level}}
-      </template>
-      <template v-slot:item.filtersEnvironment="{ item }">
-        {{item.filtersEnvironment | environment}}
-      </template>
-      <template v-slot:item.isActive="{ item }">
-        <v-icon :color="item.isActive?'success':'red'">{{(item.isActive) ? 'mdi-check-circle': 'mdi-close-circle'}}  </v-icon>
-      </template>
-      <template v-slot:item.action="{ item }">
-        <v-icon class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
-        <v-icon @click="deleteItem(item)">mdi-inbox-arrow-down</v-icon>
-      </template>
-      <template v-slot:no-data>
-        Triggers was not found
-      </template>
-    </v-data-table>
+                  <v-tab-item>
+                    <v-card>
+                      <v-card-text>
+                        <v-container>
+                          <v-row>
+                            <v-col cols="12">
+                              <v-text-field v-model="editedItem.email"  label="Email" outlined ></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                              <v-textarea outlined label="Message" v-model="editedItem.message"></v-textarea>
+                            </v-col>
+                          </v-row>
+                        </v-container>
+                      </v-card-text>
+                      <v-card-actions>
+                        <div class="flex-grow-1"></div>
+                        <v-btn color="red" text @click="close">Cancel</v-btn>
+                        <v-btn color="primary" text @click="save">Save</v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </v-tab-item>
+                </v-tabs>             
+            </v-dialog>
+          </v-toolbar>
+        </template>
+        <template v-slot:item.filtersLevel="{ item } ">
+          {{ item.filtersLevel | level}}
+        </template>
+        <template v-slot:item.filtersEnvironment="{ item } ">
+          {{ item.filtersEnvironment | environment }}
+        </template>
+        <template v-slot:item.isActive="{ item }">
+          <v-icon :color="item.isActive?'success':'red'">
+            {{ (item.isActive) ? 'mdi-check-circle': 'mdi-minus-circle' }} 
+          </v-icon>
+        </template>
+        <template v-slot:item.action="{ item }">
+          <v-icon class="mr-2" @click="editItem(item)">mdi-pencil</v-icon>
+          <v-icon @click="deleteItem(item)">mdi-inbox-arrow-down</v-icon>
+        </template>
+        <template v-slot:item.createdByFullName="{ item } ">
+          {{ item.createdByFullName }}
+          <btn  v-if="itsMe(item)" class="ma-2" color="orange" text-color="white" x-small> me </btn>
+        </template>
+        <template v-slot:no-data>
+          Triggers was not found
+        </template>
+      </v-data-table>
+    </div>
     <v-snackbar v-model="snackBar" multi-line>
       {{ snackMessage }}
       <v-btn color="red" text @click="snackBar = false">
@@ -146,7 +167,7 @@ export default {
         { text: 'Application Name', value: 'filtersAppName' },
         { text: 'Level', value: 'filtersLevel' },
         { text: 'Environment', value: 'filtersEnvironment' },
-        { text: 'Created By', value: 'createdByFullName' },
+        { text: 'Owner', value: 'createdByFullName' },
         { text: 'Actions', value: 'action', sortable: false, align: 'left' }
       ],
       editedIndex: -1,
@@ -193,6 +214,7 @@ export default {
   computed: {
     ...mapGetters('application', ['isLoading']),
     ...mapGetters('application', ['isDark']),
+    ...mapGetters('application', ['currentUser']),
     formTitle () {
       return this.isNewItem() ? 'Create Trigger' : 'Edit Trigger'
     },
@@ -205,6 +227,7 @@ export default {
           filtersAppName: el.filters.appName,
           filtersLevel: el.filters.level,
           filtersEnvironment: el.filters.environment,
+          createdById: el.createdBy.id,
           createdByFullName: el.createdBy.fullName,
           message: '',
           email: ''
@@ -229,7 +252,9 @@ export default {
         .catch(() => {
           this.setLoading(false)
         })
-
+    },
+    itsMe(item) {
+      return this.currentUser.id === item.createdById
     },
     isNewItem () {
       return (this.editedIndex === -1)

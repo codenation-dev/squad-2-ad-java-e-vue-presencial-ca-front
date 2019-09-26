5b98e9603015d4347dc933aea07fafd4d53acf72
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-26 09:06:03 -0300

    Ajustar cor secundária do tema

--- src/plugins/theme-light.js
+++ src/plugins/theme-light.js
@@ -2,6 +2,6 @@ import colors from 'vuetify/lib/util/colors'
 
 export default {
   primary: colors.indigo,
-  secondary: colors.pink,
+  secondary: colors.grey.darken3,
   tertiary: colors.purple
 }

6ad4597c272aa75a375d601b28e6abea7c65c757
Luan Eli Oliveira <loliveira@fesctecnologia.com.br>
2019-09-26 09:05:21 -0300

    Adicionar informações dos levels

--- src/domains/level.js
+++ src/domains/level.js
@@ -1,12 +1,26 @@
 export default [
   {
+    value: 'INFO',
+    text: 'Info',
+    color: 'blue-grey lighten-4 black--text',
+    icon: 'mdi-information'
+  },
+  {
     value: 'DEBUG',
-    text: 'Debug'
-  }, {
+    text: 'Debug',
+    color: 'blue darken-2 white--text',
+    icon: 'mdi-information'
+  },
+  {
     value: 'WARNING',
-    text: 'Warning'
-  }, {
+    text: 'Warning',
+    color: 'amber black--text',
+    icon: 'mdi-alert'
+  },
+  {
     value: 'ERROR',
-    text: 'Error'
-  }
+    text: 'Error',
+    color: 'red',
+    icon: 'mdi-close-circle'
+  }, 
 ]
